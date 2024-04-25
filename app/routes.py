2c2ef79d08a0c1a39481da5c91a2ac64cdc099ea
jiayang <jiayangying931@gmail.com>
2024-04-26 05:19:56 +0800

    updated login() view function for integrating with WTForms
    The get_config function retrieves the appropriate configuration class based on the environment name, falling back to the default if the name is unrecognized.

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,20 @@
-from flask import render_template
 from app import app
+from flask import render_template, flash, redirect
+from app.forms import LoginForm
 
 @app.context_processor
 def inject_global_variable():
     return dict(company="EcoHUB")
 
 @app.route('/')
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("/users/login.html", title="Home", hideNav=True)
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('users/login.html', title='Sign In', form=form)
 
 @app.route('/home')
 def home():
