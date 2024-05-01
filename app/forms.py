0b93e3c8ac2fc7676a087d97ab95ee882f358906
li wenbo <liwenbo0066@gmail.com>
2024-05-01 15:46:27 +0800

    sign-up form-handling-with-WTForms
    Create a user registration form using Flask-WTF extension.

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired,EqualTo,Length
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+class SignupForm(FlaskForm):
+    username = StringField('Username',validators=[DataRequired()])
+    password = PasswordField('Password',validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
