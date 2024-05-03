19b891abe736cde7aaa65b6bd86f566c10203afb
li wenbo <liwenbo0066@gmail.com>
2024-05-03 16:16:04 +0800

    create registration form for user
    in signup form,it includes first_name,last_name,email_address,username,password,re_password,become_seller,shop_name.submit

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired,EqualTo,Length
+from wtforms.validators import DataRequired,EqualTo,Length,ValidationError,Email
+import sqlalchemy as sa
+from app import db
+from app.models import User
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -13,3 +18,25 @@ class SignupForm(FlaskForm):
     password = PasswordField('Password',validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+class RegistrationForm(FlaskForm):
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    email_address = StringField('Email', validators=[DataRequired(), Email()])
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+    re_password = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    become_seller = BooleanField('Become a Seller')
+    shop_name = StringField('Shop Name')
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = db.session.scalar(sa.select(User).where(User.username == username.data))
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = db.session.scalar(sa.select(User).where(User.email == email.data))
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
+
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template, flash, redirect,request,jsonify,url_for
-from app.forms import LoginForm
+from app.forms import LoginForm,RegistrationForm
 import os
     
 @app.context_processor
@@ -23,15 +23,14 @@ def home():
 
 @app.route('/signup')
 def signup():
-    return render_template('/home/home.html', hideNav=True)
-@app.route('sdg_img_dirs',methods=['POST'])
+    form = RegistrationForm()
+    return render_template('/users/signup.html', form=form,hideNav=True)
+
+@app.route('/sdg_img_dirs', methods=['POST'])
 def get_sdg_img_dirs():
     data = request.json
     sdg_id = data.get('sdg_id')
     image_dirs = ['sdg_images/dir1', 'sdg_images/dir2', 'sdg_images/dir3']
     return jsonify({'image_dirs': image_dirs})
 
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
 
