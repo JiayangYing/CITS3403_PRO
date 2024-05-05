ff9dcb96f918834dc1cbfdf306b3ce02a5b252bf
jiayang <jiayangying931@gmail.com>
2024-05-05 11:41:26 +0800

    added set and check hashed password to the user calss for verification

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
-
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -22,3 +22,9 @@ class User(db.Model):
     shop_name: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,unique=True)
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
