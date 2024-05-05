6e5caacca30025cc64f2f9959b8af77f7307a578
jiayang <jiayangying931@gmail.com>
2024-05-05 21:49:16 +0800

    modified the login function for cheking user's passwords

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,13 @@ from app import app
 from flask import render_template, flash, redirect,request,jsonify,url_for
 from app.forms import LoginForm,RegistrationForm
 import os
-    
+
+from flask_login import current_user, login_user
+import sqlalchemy as sa
+from app import db
+from app.models import User
+
+
 @app.context_processor
 def inject_global_variable():
     return dict(company="EcoHUB")
@@ -10,12 +16,19 @@ def inject_global_variable():
 @app.route('/')
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('home'))
-    return render_template('users/login.html', title='Sign In', form=form, hideNav = True)
+    return render_template('users/login.html', title='Sign In', form=form)
+
 
 @app.route('/home')
 def home():
