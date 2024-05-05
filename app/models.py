4fb003062f625a382f92c2b3bf2b18c79cc3fc1a
jiayang <jiayangying931@gmail.com>
2024-05-05 11:38:58 +0800

    added more fields to user class for more information

--- app/models.py
+++ app/models.py
@@ -9,9 +9,16 @@ class User(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
                                                 unique=True)
-    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True,
+    first_name: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
+                                                unique=True)
+    last_name: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
+                                                unique=True)
+    is_seller : so.Mapped[bool] = so.mapped_column(unique=False, default=False)
+    is_active : so.Mapped[bool] = so.mapped_column(unique=False, default=True)
+    email_address: so.Mapped[str] = so.mapped_column(sa.String(120), index=True,
                                              unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
-
+    postcode: so.Mapped[int] = so.mapped_column(unique=False)
+    shop_name: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,unique=True)
     def __repr__(self):
         return '<User {}>'.format(self.username)
