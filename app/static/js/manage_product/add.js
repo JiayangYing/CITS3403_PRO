62b75588f4973473dd53d4915266ced95423478f
sleepadept <lakonawaj@gmail.com>
2024-05-09 22:22:35 +0800

    Bug and Func. Improvement
    Bug fixed: When trying to upload images, did not upload anything.
    
    Improvement:
    - Using predefined values combined with dropdown for "Category" and "Condition" inputs to prevent variations.
    - Adding dollar sign "$" on "Price"

--- app/static/js/manage_product/add.js
+++ app/static/js/manage_product/add.js
@@ -1,25 +1,38 @@
-var addProductForm = $("#addProductForm")
-
-$(() => {
-    addProductForm.find(".custom-file-input").on("change", function () {
-        const currentInput = window.$(this);
-        const fileName = window.$(this).val().split("\\").pop();
-        const assetName = currentInput.siblings(".custom-file-label").attr("for");
-        var file = this.files[0];
-        if (file) {
-            const uploadImg = new FileReader();
-            uploadImg.onload = function (displayImg) {
-                var base64 = displayImg.target.result;
-                var image = new Image();
-                image.src = base64;
-                image.onload = function () {
-                    currentInput.siblings(".custom-file-label").addClass("selected").html(fileName);
-                    currentInput.parents().siblings(".custom-file-preview").attr("src", base64);
-                    $(`#${assetName}`).val(base64);
-                };
-            }
-            uploadImg.readAsDataURL(this.files[0]);
-        }
-    });
+$(() => { 
+    document.getElementById('imageInput').addEventListener('change', handleFileSelect); 
 });
 
+function handleFileSelect(event) { 
+    const files = event.target.files; 
+    const preview = document.getElementById('imagePreview'); 
+
+    // Clear any existing content in the preview container
+    preview.innerHTML = '';
+    for (let i = 0; i < files.length; i++) { 
+        const file = files[i]; 
+
+        if (!file.type.match('image.*')) { 
+            continue; 
+        }
+
+        const reader = new FileReader(); 
+        reader.onload = function(event) { 
+            const containerHTML = `
+                <div class="form-check image-container">
+                    <label for="checkbox-img${i}">
+                        <img src="${event.target.result}" class="custom-file-preview ms-4">
+                    </label>
+                    <input type="checkbox" id="checkbox-img${i}" name="checkbox-img" class="main-image-checkbox">
+                </div>
+            `;
+            preview.innerHTML += containerHTML;
+        }; 
+
+        $(".form-check input:checkbox").on('change', function(e){
+            $(".form-check input:checkbox").prop('checked', false)
+            $(this).prop('checked', true)
+        });
+
+        reader.readAsDataURL(file); 
+    }
+}
