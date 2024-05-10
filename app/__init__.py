8f3931d7a2391ed7dcde3a143828f8b991e9f577
Lok Yx <lok@trilliongaming.com>
2024-05-10 09:15:53 +0800

    added modal error msg in form
    - UI improvement
    - dark mode improvement
    - include session for is_seller

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
-from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from config import Config
 
 
 
