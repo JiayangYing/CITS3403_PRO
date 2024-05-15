6e669b06017e0192ec07d474a46ccb877d9b1ff8
Lok Yx <lok@trilliongaming.com>
2024-05-15 16:36:11 +0800

    fix bugs for all Product functionality.
    Elasticsearch add/delete indexing to Product model has issue when performing Product related functionality.

--- app/search.py
+++ app/search.py
@@ -3,15 +3,21 @@ from flask import current_app, flash
 def add_to_index(index, model):
     if not current_app.elasticsearch:
         return
-    payload = {}
-    for field in model.__searchable__:
-        payload[field] = getattr(model, field)
-    current_app.elasticsearch.index(index=index, id=model.id, document=payload)
+    try:
+        payload = {}
+        for field in model.__searchable__:
+            payload[field] = getattr(model, field)
+        current_app.elasticsearch.index(index=index, id=model.id, document=payload)
+    except BaseException as e:
+        return
 
 def remove_from_index(index, model):
     if not current_app.elasticsearch:
         return
-    current_app.elasticsearch.delete(index=index, id=model.id)
+    try:
+        current_app.elasticsearch.delete(index=index, id=model.id)
+    except BaseException as e:
+        return
 
 
 def query_index(index, query, page, per_page):
