4558a18f918d279eb1f3177522b6b752ff710696
Lok Yx <lok@trilliongaming.com>
2024-05-19 00:24:10 +0800

    add db migration to fix conflict.
    add db migration script to fix multiple head revisions.

---
+++ migrations/versions/8ed2f9818f63_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: 8ed2f9818f63
+Revises: e735ee891fd0, e8400ed04999
+Create Date: 2024-05-19 00:23:03.119162
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8ed2f9818f63'
+down_revision = ('e735ee891fd0', 'e8400ed04999')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
